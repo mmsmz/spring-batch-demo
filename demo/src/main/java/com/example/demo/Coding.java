fb99eb1af4675e1a3683f38b8b7c2db26694697a
Mohamed Arafath <35114431+mmsmz@users.noreply.github.com>
2025-01-27 21:53:12 +0800

    initail commit

---
+++ demo/src/main/java/com/example/demo/Coding.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Coding {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
